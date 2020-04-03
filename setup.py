d26d3522c030b1f36e196d7903506dcf190a18ee
Morten Olsen Lysgaard <morten@lysgaard.no>
2020-04-03 11:20:50 +0200

    Changes to handle standard ubuntu install of libipopt

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 # for my linux distribution was buggy,
 # so by the time you read this the bugs have probably been fixed
 # and you will want to specify a different directory here.
-IPOPT_DIR = '/usr/local/'
+IPOPT_DIR = '/usr/'
 
 import os
 from distutils.core import setup
@@ -21,8 +21,8 @@ from distutils.extension import Extension
 # NumPy is much easier to install than pyipopt,
 # and is a pyipopt dependency, so require it here.
 # We need it to tell us where the numpy header files are.
-import numpy
-numpy_include = numpy.get_include()
+#import numpy
+#numpy_include = numpy.get_include()
 
 # I personally do not need support for lib64 but I'm keeping it in the code.
 def get_ipopt_lib():
@@ -37,36 +37,44 @@ if IPOPT_LIB is None:
 
 IPOPT_INC = os.path.join(IPOPT_DIR, 'include/coin/')
 
-FILES = ['src/callback.c', 'src/pyipoptcoremodule.c']
+PYIPOPT_CORE_FILES = ['src/callback.c', 'src/pyipoptcoremodule.c']
+
+# this is from: https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+from setuptools.command.build_ext import build_ext as _build_ext
+
+class build_ext(_build_ext):
+    def run(self):
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 # The extra_link_args is commented out here;
 # that line was causing my pyipopt install to not work.
 # Also I am using coinmumps instead of coinhsl.
 pyipopt_extension = Extension(
         'pyipoptcore',
-        FILES,
-        #extra_link_args=['-Wl,--rpath','-Wl,'+ IPOPT_LIB],
+        PYIPOPT_CORE_FILES,
         library_dirs=[IPOPT_LIB],
         libraries=[
-            'ipopt', 'coinblas',
-            #'coinhsl',
-            'coinmumps',
-            'coinmetis',
-            'coinlapack','dl','m',
+            'ipopt',
+            'dl',
+            'm',
             ],
-        include_dirs=[numpy_include, IPOPT_INC],
+        include_dirs=[IPOPT_INC],
         )
 
 setup(
         name="pyipopt",
-        version="0.8",
+        version="0.9",
         description="An IPOPT connector for Python",
-        author="Eric Xu",
-        author_email="xu.mathena@gmail.com",
-        url="https://github.com/xuy/pyipopt",
+        author="Morten Olsen Lysgaard",
+        author_email="molysgaard@gmail.com",
+        url="https://github.com/molysgaard/pyipopt",
+        cmdclass={'build_ext':build_ext},
+        setup_requires=['numpy',],
         packages=['pyipopt'],
-        package_dir={'pyipopt' : 'pyipoptpackage'},
-        ext_package='pyipopt',
         ext_modules=[pyipopt_extension],
+        #install_requires=['numpy',],
         )
 
